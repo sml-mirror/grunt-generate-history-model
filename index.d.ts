904f981b34473973a959a938c081326c5875377f
n.khudkov <godba11@yandex.ru>
2020-03-24 15:21:57 +0500

    add typing

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export declare function GenerateHistory(options: GenerateHistoryOptions);
+export declare function IgnoredInHistory();
+export declare function HistoryIndex(indexName?: string);
+
+export class GenerateHistoryOptions {
+    public historyPath: string;
+}
